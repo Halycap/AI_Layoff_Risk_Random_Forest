9e899f57204393edd3de8e4fd21dca6ab33a4aff
Halycap <tjtrinidadcomputer@gmail.com>
2026-04-28 16:27:33 +0200

    Add data processing block to the main for later
    WOW I SHOULD STOP, I NEED TO LET OTHER PEOPLE CONTRIBUTE

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import sklearn.ensemble import RandomForestClassifier
 from Preprocess import Process
+
+#THE PROCESS BLOCK#
+Pro=Process("data/AI_Student_Life_Pakistan_2026.csv")
+Pro.read()
+X,y=Pro.process()
+X_train,X_test,Y_train,Y_test=pro.split(0.2)

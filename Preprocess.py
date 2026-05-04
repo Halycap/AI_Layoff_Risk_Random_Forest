f2e00c0d1baf974e2f26cf69d0bec64d679e8b47
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-04 10:52:07 +0200

    finished process
    Added attributes X and y to store features and target variable. Updated process method to accept an argument for processing.

--- Preprocess.py
+++ Preprocess.py
@@ -6,13 +6,16 @@ class Process
   def __init__(self, path):
     self.path=path
     self.dataset=None
+    self.X=None
+    self.y=None
     
   def read(self):
     self.dataset=pd.read_csv(self.path)
     return self.dataset
 
-  def process(self):
-    #TO DO#
+  def process(self,ans):
+      self.X = self.df.drop(ans, axis=1,)
+      self.y = self.df[ans]
   
   def split(X,Y,ratio):
     return train_test_split(X,Y,test_size=ratio,random_state=42)

47a5763acec82f6907450d45102370baf94a6937
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-04 16:46:58 +0200

    made def for ease of testing
    just to make it easy to import and run

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from Preprocess import Process
+import os
 
 #THE PROCESS BLOCK#
-#Loads the class and inputs the path#
-Pro=Process("data/AI_Student_Life_Pakistan_2026.csv")
-#Reads said file to process#
-Pro.read()
-#Displays the features and asks user to input a feature for prediction#
-Pro.features()
-target=ask_target()
-#extracts X and y as well as split it to prepare to put in scikit
-Pro.process(target)
-X_train,X_test,Y_train,Y_test=pro.split(0.2)
+def dataprocess():
+  default_path = "data/AI_Student_Life_Pakistan_2026.csv"
+  
+  file_name = input("CSV file name, or press Enter for default: ")
+  
+  if file_name == "":
+    path = default_path
+  else:
+    path = os.path.join("data/", file_name)
+  
+  if not os.path.exists(path):
+    print("File not found. Using default dataset.")
+    path = default_path
+    
+  #Loads the class and inputs the path#
+  Pro=Process(path)
+  #Reads said file to process#
+  Pro.read()
+  #Displays the features and asks user to input a feature for prediction#
+  Pro.features()
+  target=Pro.ask_target()
+  #extracts X and y as well as split it to prepare to put in scikit
+  Pro.process(target)
+  X_train,X_test,Y_train,Y_test=Pro.split(0.2)
+  return 0
 
 #training

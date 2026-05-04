c9599475a6aa7fc1890ed73b5148a41ab7c5fdce
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-04 11:09:10 +0200

    Displaying how the code works with comments
    And fixing the fact i forgot to put x and y in split

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import sklearn.ensemble import RandomForestClassifier
 from Preprocess import Process
 
 #THE PROCESS BLOCK#
+#Loads the class and inputs the path#
 Pro=Process("data/AI_Student_Life_Pakistan_2026.csv")
+#Reads said file to process#
 Pro.read()
+#Displays the features and asks user to input a feature for prediction#
 Pro.features()
 target=ask_target()
+#extracts X and y as well as split it to prepare to put in scikit
 X,y=Pro.process(target)
-X_train,X_test,Y_train,Y_test=pro.split(0.2)
+X_train,X_test,Y_train,Y_test=pro.split(X,y,0.2)
+
+#training

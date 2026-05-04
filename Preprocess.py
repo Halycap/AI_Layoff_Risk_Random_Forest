d8f15a6756b7210843406b800693eedf60198683
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-04 11:11:15 +0200

    changed the process from self to output.
    its so that it works better with the original code. should probably just change that instead and keep everything to self but this works

--- Preprocess.py
+++ Preprocess.py
@@ -6,8 +6,6 @@ class Process
   def __init__(self, path):
     self.path=path
     self.dataset=None
-    self.X=None
-    self.y=None
     
   def read(self):
     self.dataset=pd.read_csv(self.path)
@@ -23,8 +21,9 @@ class Process
     return target_column
 
   def process(self,ans):
-      self.X = self.df.drop(ans, axis=1,)
-      self.y = self.df[ans]
+      X = self.df.drop(ans, axis=1,)
+      y = self.df[ans]
+      return X,y
   
   def split(X,Y,ratio):
     return train_test_split(X,Y,test_size=ratio,random_state=42)

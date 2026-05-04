2a77011c16bd1070775ae5f22dc94ebd21f72faa
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-04 11:03:50 +0200

    able to customize the target to extract

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sklearn.ensemble import RandomForestClassifier
 from Preprocess import Process
 
-target="x"
 #THE PROCESS BLOCK#
 Pro=Process("data/AI_Student_Life_Pakistan_2026.csv")
 Pro.read()
+Pro.feature()
+target=ask_target()
 X,y=Pro.process(target)
 X_train,X_test,Y_train,Y_test=pro.split(0.2)

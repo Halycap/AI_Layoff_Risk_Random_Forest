9160a9e462f87b3b8f4f6cf08e121b33d1a5b726
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-04 11:04:09 +0200

    Fix method name from feature to features

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Preprocess import Process
 #THE PROCESS BLOCK#
 Pro=Process("data/AI_Student_Life_Pakistan_2026.csv")
 Pro.read()
-Pro.feature()
+Pro.features()
 target=ask_target()
 X,y=Pro.process(target)
 X_train,X_test,Y_train,Y_test=pro.split(0.2)

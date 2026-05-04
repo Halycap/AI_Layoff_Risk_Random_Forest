ea8793127bbf60d57ea71729655d06180309cbad
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-04 10:53:13 +0200

    implemented process to be able to find any column

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sklearn.ensemble import RandomForestClassifier
 from Preprocess import Process
 
+target="x"
 #THE PROCESS BLOCK#
 Pro=Process("data/AI_Student_Life_Pakistan_2026.csv")
 Pro.read()
-X,y=Pro.process()
+X,y=Pro.process(target)
 X_train,X_test,Y_train,Y_test=pro.split(0.2)

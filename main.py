b942497267e4fcf935f71e40e0162ae3a4893c57
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-11 13:26:33 +0200

    Add RandomForestClassifier for model training
    as the ai describes. im too lazy to make a title xD

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ Pro=Process(path)
 Pro.process(feature_predict,training_ratio)
 
 #TRAINING BLOCK
+tree=RandomForestClassifier(n_estimators=100, random_state=42)
 
+model.fit(Pro.X_train, Pro.y_train)
 
+predict= model.predict(Pro.X_test)
+
+accuracy= model.accuracy(Pro.y_test,predict)
 #OUTPUT BLOCK

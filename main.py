c6a6c2a6c87e53b59d4bd36ead7e250c1ba8aef7
Vivifairy20 <tuncayenya@gmail.com>
2026-05-11 13:51:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,14 @@ predict= model.predict(Pro.X_test)
 
 accuracy= model.accuracy(Pro.y_test,predict)
 #OUTPUT BLOCK
+
+# Image Block
+plt.figure(figsize=(20,10)) 
+plot_tree(
+  model.estimators_[0],
+  feature_names=Pro.X.columns,
+  class_names=[str(c) for c in model.classes_],
+  filled=True,
+  rounded=True,
+  max_depth=3
+)

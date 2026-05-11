26e5f381e0eb1314e011ac14a646b7c250a5874a
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-11 11:30:39 +0200

    Cleaned a lot of typos, and finished filter which is now clean
    yes half of it is human made errors, i just plugged my code to ai to double check for errors. i am sorry for whoever has to read my commits

--- Preprocess.py
+++ Preprocess.py
@@ -1,13 +1,13 @@
 #Where we put the data processing code#
 import pandas as pd
-from sklearn.model_selection import train_test_split:
+from sklearn.model_selection import train_test_split
 
-class Process
+class Process:
   def __init__(self, path):
     self.path=path
     self.dataset=None
     self.X=None
-    self.Y=None
+    self.y=None
     self.X_train = None
     self.X_test = None
     self.y_train = None
@@ -17,12 +17,22 @@ class Process
     self.dataset=pd.read_csv(self.path)
     return print("read success")
     
-  def filter(self):
-    self.dataset
+  def clean(self):
+    for column in self.dataset.columns:
+        if column == target:
+            continue
+
+        unique_count = self.dataset[column].nunique()
+        unique_ratio = unique_count / rows
+
+        if unique_ratio >= threshold:
+            self.dataset = self.dataset.drop(column, axis=1)
+            print(f"Dropped {column}")
+          
     return print("filter success")
     
   def allocate(self,ans):
-    self.X = self.df.drop(ans, axis=1,)
+    self.X = self.dataset.drop(ans, axis=1,)
     self.y = self.df[ans]
     return print("allocated X and y")
   
@@ -30,12 +40,12 @@ class Process
     self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,self.y,test_size=ratio,random_state=42)
     return print("Dataset split for testing and training")
 
-  def process(self,y,ratio):
+  def process(self,target,ratio):
     print("reading Dataset")
-    read(self)
+    self.read()
     print("filtering Dataset for unuseable features")
-    filter(Self)
+    self.filter()
     print("allocating X and y")
-    allocate(self,y)
+    self.allocate(target)
     print("splitting for testing and training")
-    split(self,ratio)
+    self.split(ratio)

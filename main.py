99b9ceb809781e171de6507fe8709b643c9b5c97
Halycap <tjtrinidadcomputer@gmail.com>
2026-05-12 15:07:24 +0200

    My bad
    I named the model tree, forgot that its the object.method style type syntax and just did the whole model by default cause i was using references to write the code XD

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ Pro.process(feature_predict,test_ratio,random_split)
 #TRAINING BLOCK
 tree=RandomForestClassifier(n_estimators=tree_size, random_state=random_train)
 
-model.fit(Pro.X_train, Pro.y_train)
+tree.fit(Pro.X_train, Pro.y_train)
 
 #OUTPUT BLOCK
-predict= model.predict(Pro.X_test)
+predict= tree.predict(Pro.X_test)
 
-accuracy= model.accuracy(Pro.y_test,predict)
+accuracy= tree.accuracy(Pro.y_test,predict)
 
 # Image Block for modeling
 plt.figure(figsize=(20,10)) 
 plot_tree(
-  model.estimators_[0],
+  tree.estimators_[0],
   feature_names=Pro.X.columns,
-  class_names=[str(c) for c in model.classes_],
+  class_names=[str(c) for c in tree.classes_],
   filled=True,
   rounded=True,
   max_depth=3

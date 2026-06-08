6d0048974262dae71533e758c000c73bb8a7975e
Halycap <tjtrinidadcomputer@gmail.com>
2026-06-08 16:13:58 +0200

    added more comments
    same reason as last commit. that and i also simpliied the datatype i think. way less bloat

--- Preprocess.py
+++ Preprocess.py
@@ -13,18 +13,17 @@ class Process:
     self.X_test = None
     self.y_train = None
     self.y_test = None
+
   #This one is utils. but i dont wanna make a utils folder 0v0
-  
   def data_type_identifier(self, target):
     y = self.dataset[target]
-    unique_count = y.nunique()
-    is_numeric = is_numeric_dtype(y)
 
-    if not is_numeric:
+    if not is_numeric_dtype(y):
         return "category"
     return "value"
     
   def clean_column(self,target,threshold=0.8):
+    #clean columns cleans columns that has too many unique entries(treshold) and takes a treshold value to cut off certain columms
     rows = len(self.dataset)
     for column in self.dataset.columns:
       if column == target:
@@ -40,6 +39,7 @@ class Process:
     return print("column filter success")
     
   def clean_row(self,target,missing_limit=0.1):
+    #cleans rows that have missing_limit amount of missing entries
     self.dataset = self.dataset.dropna(subset=[target])
     
     feature_columns = self.dataset.columns.drop(target)
@@ -52,10 +52,12 @@ class Process:
     return print("row filter success")
 
   def clean(self, target,missing_limit):
+    # combines cleaning rows and columns
     self.clean_column(target)
     self.clean_row(target,missing_limit)
 
   def fill(self, target):
+    # if relevant, fills up remaining missing entries to median
     feature_columns = self.dataset.columns.drop(target)
 
     for column in feature_columns:
@@ -71,6 +73,8 @@ class Process:
     return print("missing entries filled")
   
   def correlate(self, target, threshold):
+    # removes columns that dont correlate with a target
+    # it cannot remove columns if the target is not a value or the columns tested are categories
     target_type = self.data_type_identifier(target)
 
     # Only use correlation filter for regression/value targets
@@ -88,12 +92,12 @@ class Process:
 
     weak_columns = correlations[correlations < threshold].index
 
-    # Never drop the target column
+    # never drop the target value, i learnt my lesson
     weak_columns = weak_columns.drop(target, errors="ignore")
 
     self.dataset = self.dataset.drop(columns=weak_columns)
 
-    print(f"Dropped weakly correlated columns: {list(weak_columns)}")
+    print(f"Dropped columns: {list(weak_columns)}")
     
   def allocate(self,ans):
     self.X = self.dataset.drop(ans, axis=1,)
@@ -105,10 +109,12 @@ class Process:
     return print("allocated X and y")
   
   def split(self, ratio,random):
+    #train and test values now being allocated
     self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,self.y,test_size=ratio,random_state=random)
     return print("Dataset split for testing and training")
 
   def process(self,target,ratio,random=42,correlation=0.1,missing_limit=0.5):
+    # the mega function. this way only one function is called outside instead of 5 lines.
     print("filtering Dataset for unuseable features")
     self.clean(target,missing_limit)
 
